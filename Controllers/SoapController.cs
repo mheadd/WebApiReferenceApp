936ba954efd47bcc5a9dbe82c5c683b61b919bd0
Mark Headd <mark.headd@gsa.gov>
2017-10-27 12:33:07 -0400

    Initial commit

---
+++ Controllers/SoapController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using Microsoft.AspNetCore.Mvc;
+using SoapHttpClient;
+using SoapHttpClient.Extensions;
+using SoapHttpClient.Enums;
+using Newtonsoft.Json;
+
+namespace WebApiReferenceApp.Controllers
+{
+    [Route("api/[controller]")]
+    public class SoapController : Controller
+    {
+        private XNamespace ns = XNamespace.Get("http://helio.spdf.gsfc.nasa.gov/");
+        private Uri endpoint = new Uri("http://sscweb.gsfc.nasa.gov:80/WS/helio/1/HeliocentricTrajectoriesService");
+        private string methodName = "getAllObjects";
+
+        // GET api/soap
+        [HttpGet]
+        public ContentResult Get()
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(CallNasaAsync().Result);
+
+            return Content(JsonConvert.SerializeObject(doc), "application/json");
+        }
+        private async Task<string> CallNasaAsync()
+        {
+            var body = new XElement(ns.GetName(methodName)) ;
+
+            using (var soapClient = new SoapClient())
+            {
+                var response =
+                  await soapClient.PostAsync(
+                          endpoint: endpoint,
+                          soapVersion: SoapVersion.Soap11,
+                          body: body);
+
+                return await response.Content.ReadAsStringAsync();
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,16 @@
+# Web API Reference Application
+
+A simple .NET Core Web API application to demonstrate how to connect to various backend data sources (SOAP, REST, RDBMS, etc.)
+
+## Connecting to a SOAP Service
+
+This example uses the [`SoapHttpCLient`](https://www.nuget.org/packages/SoapHttpClient/) and borrows heavily from [that project's documentation](https://github.com/pmorelli92/SoapHttpClient). It shows how to connect to NASA's Heliocentric Trajectories Web Services and uses the [`Newtonsoft.Json` package](https://www.nuget.org/packages/Newtonsoft.Json/) to convert the XML SOAP response to JSON format.
+
+## Connecting to a REST Service
+
+Coming soon.
+
+## Connecting to a RDBMS
+
+Coming soon.
+
