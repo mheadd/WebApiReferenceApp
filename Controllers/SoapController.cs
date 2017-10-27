40726e56ca032d04880cde9505e571a7b6420642
Mark Headd <mark.headd@gsa.gov>
2017-10-27 12:38:32 -0400

    Add comments

--- Controllers/SoapController.cs
+++ Controllers/SoapController.cs
@@ -14,6 +14,7 @@ namespace WebApiReferenceApp.Controllers
     [Route("api/[controller]")]
     public class SoapController : Controller
     {
+        // Details of NASA's NASA's Heliocentric Trajectories SOAP Service
         private XNamespace ns = XNamespace.Get("http://helio.spdf.gsfc.nasa.gov/");
         private Uri endpoint = new Uri("http://sscweb.gsfc.nasa.gov:80/WS/helio/1/HeliocentricTrajectoriesService");
         private string methodName = "getAllObjects";
@@ -22,15 +23,24 @@ namespace WebApiReferenceApp.Controllers
         [HttpGet]
         public ContentResult Get()
         {
+            // Create a new XML document to hold the response from the SOAP service.
             XmlDocument doc = new XmlDocument();
+
+            // Load the response from the SOAP service.
             doc.LoadXml(CallNasaAsync().Result);
 
+            // Convert the XML document to JSON format.
             return Content(JsonConvert.SerializeObject(doc), "application/json");
         }
+        /**
+         * Private method to call SOAP service.
+         */
         private async Task<string> CallNasaAsync()
         {
-            var body = new XElement(ns.GetName(methodName)) ;
+            // Construct the SOAP body
+            var body = new XElement(ns.GetName(methodName));
 
+            // Make the call to the SOAP service.
             using (var soapClient = new SoapClient())
             {
                 var response =
